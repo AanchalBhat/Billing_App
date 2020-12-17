9b69f81d8e41069834557ecf50b1c74c1ecc45bc
Aanchalbhat <aanchalbhat1996@gmail.com>
2020-12-17 18:08:00 +0530

    ff

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL =  process.env.BACK_API_URL ? process.env.BACK_API_URL :'http://localhost:8000/api/v1'
-const BASE_URL =  process.env.BACK_API_URL 
+const BASE_URL = 'https://billing-node.herokuapp.com/api/v1 '
 
 export const Axios = axios.create({
     baseURL: BASE_URL,
