207175a4b5919368015603dc25d276f526fd762a
Aanchalbhat <aanchalbhat1996@gmail.com>
2020-12-17 18:01:52 +0530

    ff

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL =  process.env.BACK_API_URL ? process.env.BACK_API_URL :'http://localhost:8000/api/v1'
+// const BASE_URL =  process.env.BACK_API_URL ? process.env.BACK_API_URL :'http://localhost:8000/api/v1'
+const BASE_URL =  process.env.BACK_API_URL 
 
 export const Axios = axios.create({
     baseURL: BASE_URL,
